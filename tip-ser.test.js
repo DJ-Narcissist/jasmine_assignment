49d7ba6d7bd3b65368d0ac39f2a314cdc3162758
CWise <CWise2999@gmail.com>
2023-06-06 12:15:23 -0400

    test and more js

---
+++ tip-ser.test.js
@@ -0,0 +1,8 @@
+describe ("Servers test (with setup and tear-down"), function(){
+    beforeEach(function(){
+        serverNameinput.value ='Alice';
+    });
+}
+
+
+it ('should add a new server to allServers on submitServerInfo()',)
